db8934b2b265bc54a8558e1086567c6de6beabb6
Nikhil-Yadav97 <nikhil.yadav.9780@gmail.com>
2025-07-07 23:20:58 +0530

    Enhance index.html layout and add Bootstrap JS reference in script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
 document.getElementById("btn").addEventListener(('click'),()=>{
 
     document.getElementById('scroll').scrollIntoView({behavior:'smooth'})
+    // <!-- Bootstrap 5 JS (optional for components like modals, dropdowns, etc.) -->
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+
 })
